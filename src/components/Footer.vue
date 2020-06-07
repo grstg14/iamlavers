5c4c1623d1d25ae0d181df1225b50a91ec94172d
hemmephi <philipp.hemmer@informance.it>
2020-06-07 17:20:16 +0200

    themes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,36 @@
 <template>
-  <div>
+  <div> 
     <v-container fluid>
       <v-row justify="center">
         <v-col lg="12">
-          <a href="https://www.sissirecords.com/" target="_blank"><v-img max-width="50px" :src="require('@/assets/sr.png')" /></a>
+          <a href="https://www.sissirecords.com/" target="_blank"><v-img max-width="80px" :src="require('@/assets/sr.png')" :class="invertingClass" /></a>
         </v-col>
 
       </v-row>
     </v-container>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    small() {
+      return (this.$vuetify.breakpoint.xs || this.$vuetify.breakpoint.s) ? true : false;
+    },
+
+    invertingClass() {
+      if (!this.small) {
+        return "inverted"
+      } else {
+        return ""
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .inverted {
+    filter: invert(1);
+  }
+</style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,21 +3,67 @@
   <v-container fluid>
     <v-row justify="center">
       <v-col lg="4">
-        <v-btn href="https://lnk.to/LAVERS" target="_blank" :ripple="false" block small text class="navButton text-left">MUSIC</v-btn>
+        <v-btn 
+          href="https://lnk.to/LAVERS" 
+          target="_blank" 
+          :ripple="false" 
+          :dark="!small" 
+          :x-large="!small"
+          :small="small" 
+          block 
+          text 
+          class="navButton">
+            MUSIC
+          </v-btn>
       </v-col>
       <v-col lg="4">
-        <v-btn href="https://www.aexattack.com" target="_blank" :ripple="false" block small text class="navButton">2020</v-btn>
+        <v-btn 
+          href="https://www.aexattack.com"
+          target="_blank" 
+          :ripple="false" 
+          :dark="!small" 
+          :x-large="!small"
+          :small="small" 
+          block 
+          text 
+          class="navButton"
+        >
+          2020
+        </v-btn>
       </v-col>
       <v-col lg="4">
-        <v-btn href="https://www.instagram.com/axl_aex_vers" target="_blank" :ripple="false" block small text class="navButton">CONTACT</v-btn>
+        <v-btn 
+          href="https://www.instagram.com/axl_aex_vers"
+          target="_blank" 
+          :ripple="false" 
+          :dark="!small" 
+          :x-large="!small"
+          :small="small" 
+          block 
+          text 
+          class="navButton"
+        >CONTACT</v-btn>
       </v-col>
     </v-row>
 </v-container>
 </div>
 </template>
-
+<script>
+export default {
+  computed: {
+    small() {
+      if (this.$vuetify.breakpoint.xs || this.$vuetify.breakpoint.s) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+}
+</script>
 <style scoped>
   .navButton {
     font-family: LetterGothicStd;
+    font-weight:bold;
   }
 </style>
