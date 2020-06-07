58616dc4ec6d981a8823e55b576634c19c11f1b8
hemmephi <philipp.hemmer@informance.it>
2020-06-07 16:48:51 +0200

    btn size

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,13 +3,13 @@
   <v-container fluid>
   <v-row justify="center">
     <v-col lg="4">
-      <v-btn href="https://lnk.to/LAVERS" target="_blank" :ripple="false" block large text class="navButton text-left">MUSIC</v-btn>
+      <v-btn href="https://lnk.to/LAVERS" target="_blank" :ripple="false" block small text class="navButton text-left">MUSIC</v-btn>
     </v-col>
     <v-col lg="4">
-      <v-btn href="https://www.aexattack.com" target="_blank" :ripple="false" block large text class="navButton">2020</v-btn>
+      <v-btn href="https://www.aexattack.com" target="_blank" :ripple="false" block small text class="navButton">2020</v-btn>
     </v-col>
     <v-col lg="4">
-      <v-btn href="https://www.instagram.com/axl_aex_vers" target="_blank" :ripple="false" block large text class="navButton">CONTACT</v-btn>
+      <v-btn href="https://www.instagram.com/axl_aex_vers" target="_blank" :ripple="false" block small text class="navButton">CONTACT</v-btn>
     </v-col>
   </v-row>
 </v-container>
@@ -18,7 +18,6 @@
 
 <style scoped>
   .navButton {
-    font-size:1.5em;
     font-family: LetterGothicStd;
   }
 </style>
